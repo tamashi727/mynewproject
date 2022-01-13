c0e1c87106473128928447667692969e44543bb7
tamashi727 <ctamanna445@gmail.com>
2022-01-13 15:31:41 +0600

    admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,12 @@
 <x-app-layout>
     <h1>this is admin dashboard</h1>
 </x-app-layout>
+@extends('layouts.admin')
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h1>Me color</h1>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+
+        <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+   
+    <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet">
+        
+        
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}"> 
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+
+
+    </head>
+    <body class="font-sans antialiased g-sidenav-show  bg-gray-200">
+        @include('layouts.inc.sidebar')
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+            @include('layouts.inc.adminnav')
+            <div class="container-fluid py-4">
+                @yield('content')
+            </div>
+            @include('layouts.inc.adminfooter')
+    
+        </main>
+
+
+        
+        @stack('modals')
+
+        @livewireScripts
+
+        <!-- Scripts -->
+        <script src="{{asset ('admin/assets/js/core/popper.min.js')}}"></script>
+        <script src="{{asset ('admin/assets/js/core/bootstrap.min.js')}}"></script>
+        <script src="{{asset ('admin/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
+        <script src="{{asset ('admin/assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
+        <script src="{{asset ('admin/assets/js/plugins/chartjs.min.js')}}"></script>
+
+        <script>
+            var ctx = document.getElementById("chart-bars").getContext("2d");
+        
+            new Chart(ctx, {
+              type: "bar",
+              data: {
+                labels: ["M", "T", "W", "T", "F", "S", "S"],
+                datasets: [{
+                  label: "Sales",
+                  tension: 0.4,
+                  borderWidth: 0,
+                  borderRadius: 4,
+                  borderSkipped: false,
+                  backgroundColor: "rgba(255, 255, 255, .8)",
+                  data: [50, 20, 10, 22, 50, 10, 40],
+                  maxBarThickness: 6
+                }, ],
+              },
+              options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                  legend: {
+                    display: false,
+                  }
+                },
+                interaction: {
+                  intersect: false,
+                  mode: 'index',
+                },
+                scales: {
+                  y: {
+                    grid: {
+                      drawBorder: false,
+                      display: true,
+                      drawOnChartArea: true,
+                      drawTicks: false,
+                      borderDash: [5, 5],
+                      color: 'rgba(255, 255, 255, .2)'
+                    },
+                    ticks: {
+                      suggestedMin: 0,
+                      suggestedMax: 500,
+                      beginAtZero: true,
+                      padding: 10,
+                      font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                      },
+                      color: "#fff"
+                    },
+                  },
+                  x: {
+                    grid: {
+                      drawBorder: false,
+                      display: true,
+                      drawOnChartArea: true,
+                      drawTicks: false,
+                      borderDash: [5, 5],
+                      color: 'rgba(255, 255, 255, .2)'
+                    },
+                    ticks: {
+                      display: true,
+                      color: '#f8f9fa',
+                      padding: 10,
+                      font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                      },
+                    }
+                  },
+                },
+              },
+            });
+        
+        
+            var ctx2 = document.getElementById("chart-line").getContext("2d");
+        
+            new Chart(ctx2, {
+              type: "line",
+              data: {
+                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                datasets: [{
+                  label: "Mobile apps",
+                  tension: 0,
+                  borderWidth: 0,
+                  pointRadius: 5,
+                  pointBackgroundColor: "rgba(255, 255, 255, .8)",
+                  pointBorderColor: "transparent",
+                  borderColor: "rgba(255, 255, 255, .8)",
+                  borderColor: "rgba(255, 255, 255, .8)",
+                  borderWidth: 4,
+                  backgroundColor: "transparent",
+                  fill: true,
+                  data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+                  maxBarThickness: 6
+        
+                }],
+              },
+              options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                  legend: {
+                    display: false,
+                  }
+                },
+                interaction: {
+                  intersect: false,
+                  mode: 'index',
+                },
+                scales: {
+                  y: {
+                    grid: {
+                      drawBorder: false,
+                      display: true,
+                      drawOnChartArea: true,
+                      drawTicks: false,
+                      borderDash: [5, 5],
+                      color: 'rgba(255, 255, 255, .2)'
+                    },
+                    ticks: {
+                      display: true,
+                      color: '#f8f9fa',
+                      padding: 10,
+                      font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                      },
+                    }
+                  },
+                  x: {
+                    grid: {
+                      drawBorder: false,
+                      display: false,
+                      drawOnChartArea: false,
+                      drawTicks: false,
+                      borderDash: [5, 5]
+                    },
+                    ticks: {
+                      display: true,
+                      color: '#f8f9fa',
+                      padding: 10,
+                      font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                      },
+                    }
+                  },
+                },
+              },
+            });
+        
+            var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
+        
+            new Chart(ctx3, {
+              type: "line",
+              data: {
+                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                datasets: [{
+                  label: "Mobile apps",
+                  tension: 0,
+                  borderWidth: 0,
+                  pointRadius: 5,
+                  pointBackgroundColor: "rgba(255, 255, 255, .8)",
+                  pointBorderColor: "transparent",
+                  borderColor: "rgba(255, 255, 255, .8)",
+                  borderWidth: 4,
+                  backgroundColor: "transparent",
+                  fill: true,
+                  data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                  maxBarThickness: 6
+        
+                }],
+              },
+              options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                  legend: {
+                    display: false,
+                  }
+                },
+                interaction: {
+                  intersect: false,
+                  mode: 'index',
+                },
+                scales: {
+                  y: {
+                    grid: {
+                      drawBorder: false,
+                      display: true,
+                      drawOnChartArea: true,
+                      drawTicks: false,
+                      borderDash: [5, 5],
+                      color: 'rgba(255, 255, 255, .2)'
+                    },
+                    ticks: {
+                      display: true,
+                      padding: 10,
+                      color: '#f8f9fa',
+                      font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                      },
+                    }
+                  },
+                  x: {
+                    grid: {
+                      drawBorder: false,
+                      display: false,
+                      drawOnChartArea: false,
+                      drawTicks: false,
+                      borderDash: [5, 5]
+                    },
+                    ticks: {
+                      display: true,
+                      color: '#f8f9fa',
+                      padding: 10,
+                      font: {
+                        size: 14,
+                        weight: 300,
+                        family: "Roboto",
+                        style: 'normal',
+                        lineHeight: 2
+                      },
+                    }
+                  },
+                },
+              },
+            });
+          </script>
+          <script>
+            var win = navigator.platform.indexOf('Win') > -1;
+            if (win && document.querySelector('#sidenav-scrollbar')) {
+              var options = {
+                damping: '0.5'
+              }
+              Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+            }
+          </script>
+          <script src="{{ asset('admin/assets/js/material-dashboard.min.js?v=3.0.0') }}"></script>
+          
+          @yield('scripts')
+    </body>
+</html>
